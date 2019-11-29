d9e0b637773a50a678f80167abcc81c730ca540d
Brunodev09 <bruno.mayol.giannotti@gmail.com>
2019-11-29 19:55:05 -0300

    Update game.js

--- game.js
+++ game.js
@@ -1,66 +1,75 @@
 function checkPalindrome(inputString) {
-    let word = inputString.split('');
-    let rev = inputString.split('').reverse();
-    for (var i = 0; i < word.length; i++) {
-        if (word[i] != rev[i]) {
-            return false;
-        }
-    }
-    return true;
+    return inputString.split('').reverse().join('') === inputString;
 }
 
-function adjacentElementsProduct(inputArray) {
-    let aux;
-    let lg = -Infinity;
-    for (var i = 0; i < inputArray.length; i++) {
-        aux = inputArray[i] * inputArray[i + 1];
-        if (lg < aux) lg = aux;
-    }
-    return lg;
+function isSequence(arr, index) {
+	let aux = [...arr];
+	aux.splice(index, 1);
+	console.log(aux);
+	for (let j = 1; j < aux.length - 1; j++) {
+		if (aux[j] >= aux[j + 1] || aux[j] <= aux[j - 1]) return false;
+	}
+	return true;
 }
 
-
-function shapeArea(n) {
-    if (n == 1) return n;
-    else return n * (n + 4);
+function adjacent(arr) {
+	let bool;
+	for (let i = 1; i < arr.length - 1; i++) {
+		if (arr[i] >= arr[i + 1]) {
+			bool = isSequence(arr, i);
+			if (!bool) return false;
+		} else if (arr[i] <= arr[i - 1]) {
+			bool = isSequence(arr, i - 1);
+			if (!bool) return false;
+		}
+	}
+	return true;
 }
 
-// function reverseParentheses(s) {
-//     let aux = s.split('');
-//     let c = s;
-//     let clean = c.replace(/[^a-zA-Z ]/g, "");
-//     let timesToRun = (aux.length - (aux.toString().replace(/[^a-zA-Z ]/g, "").length)) / 2;
-//     let start = s.indexOf('(');
-//     let end = s.lastIndexOf(')');
-//     let edited = aux.slice(start+1,end);
-//     timesToRun--;
-//     while(timesToRun > 0) {
-//         start = edited.indexOf('(');
-//         end = edited.lastIndexOf(')');
-//         test = edited.slice(start+1,end);
-//         timesToRun--;
-//     }
-// }
-console.log(reverseParentheses('a(bcdefghijkl(mno)p)q'));
-
-function reverseParentheses(s) {
-    let start, end;
-    let aux = s.split('');
-    let indexes = [];
-    let timesToRun = (aux.length - (aux.toString().replace(/[^a-zA-Z ]/g, "").length)) / 2;
-    aux.filter((element, index) => {
-        if(element === '(' || element === ')') {
-            indexes.push(index);
-        }
-    });
-    while (timesToRun > 0) {
-        start = aux.lastIndexOf('(');
-        end = aux.indexOf(')')
-        timesToRun--;
-    }
+function maxStatues(arr) {
+	arr.sort((a, b) => a - b);
+	let c = 0;
+	console.log(arr);
+	for (let i = 0; i < arr.length - 1; i++) {
+		if (arr[i + 1] - arr[i] > 1) c += arr[i + 1] - arr[i] - 1;
+	}
+	return c;
 }
 
-//.replace(/[^a-zA-Z ]/g, "")
+function square(n) {
+	if (n === 1) return n;
+	let acc = 1;
+	for (let i = 1; i <= n; i++) {
+		acc += 4 * i;
+		console.log(acc);
+	}
+	return acc;
+}
+
+function matrixes(arr) {
+	let sum = 0;
+	for (let i = 1; i < arr.length; i++) {
+		for (let j = 0; j < arr[i].length; j++) {
+			console.log(arr);
+			if (!arr[i - 1][j]) arr[i][j] = 0;
+		}
+	}
+	for (let i = 0; i < arr.length; i++) {
+		arr[i] = arr[i].filter(k => k);
+	}
+	arr.map(k => k.map(w => (sum += w)));
+	console.log(arr, sum);
+	return sum;
+}
+
+console.log(
+	matrixes([
+		[0, 1, 1, 2],
+		[0, 5, 0, 0],
+		[2, 0, 3, 3]
+	])
+);
+
 
 
 
