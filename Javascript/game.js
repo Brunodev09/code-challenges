85b304f3664e9e2fca7476846457013d9f1aca45
Brunodev09 <bruno.mayol.giannotti@gmail.com>
2020-02-19 14:08:29 -0300

    Starting matrix rotation problem.

--- Javascript/game.js
+++ Javascript/game.js
@@ -1075,3 +1075,43 @@ let Benchmark = require('./Benchmark');
 
 // let factorialTest = new Benchmark(fibonacci, test);
 // console.log(factorialTest.get())
+
+
+
+// You are given an n x n 2D matrix representing an image.
+// Rotate the image by 90 degrees (clockwise).
+// Note:
+// You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+// Given input matrix = 
+// [
+//   [1,2,3],
+//   [4,5,6],
+//   [7,8,9]
+// ],
+
+// rotate the input matrix in-place such that it becomes:
+// [
+//   [7,4,1],
+//   [8,5,2],
+//   [9,6,3]
+// ]
+
+function createMatrix(n) {
+	let m = new Array(n);
+	for (let i = 0; i < n; i++) {
+		m[i] = new Array(n);
+	}
+	return m;
+}
+
+var rotate = (matrix) => {
+	let rotatedMatrix = createMatrix(matrix.length);
+	for (let i = 0; i < matrix.length; i++) {
+		for (let j = 0; j < matrix[i].length; j++) {
+			rotatedMatrix[j][i]
+		}
+	}
+};
+
+console.log(rotate([[1,2,3],[4,5,6],[7,8,9]]));
